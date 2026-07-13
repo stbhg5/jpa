74df07d55232c759f7d94e86d3d7a9aea096c071
jint <stbhg5@gmail.com>
2026-07-13 18:40:16 +0900

    20260713 영속성 컨텍스트2

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -15,6 +15,14 @@ public class Member {
     // @Column(name = "username") // 컬럼명 매핑 가능
     private String name;
 
+    // 엔티티 클래스 생성자 사용시, JPA 는 내부적으로 리플렉션을 쓰며 동적으로 객체생성을 하기 때문에, 기본 생성자가 필요하다. (접근 제어자는 상관 없음)
+    public Member() {}
+
+    public Member(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
     // Getter, Setter
     public Long getId() {
         return id;
